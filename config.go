8c7c75386013f0d6eb5488a12ebc7f7a29704963
Yuta Saito <uc4w6c@bma.biglobe.ne.jp>
2023-06-27 23:18:26 +0900

    read yaml

---
+++ config.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+        "os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Deployment struct {
+  Namespace string
+  Name string
+  Replicas int
+}
+
+type Config struct {
+  Deployments []Deployment
+}
+
+func getConfig() Config {
+  config := Config{}
+  b, _ := os.ReadFile("config.yaml")
+
+  err := yaml.Unmarshal(b, &config)
+  if err != nil {
+      panic(err)
+  }
+  return config
+}
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+    fmt.Println(time.Now())
+
+    config := getConfig()
+    for _, v := range config.Deployments {
+        fmt.Println(v.Name + "," + v.Namespace)
+    }
+}
+
